29bb177f2c3659246756c08d6feaf2d13cff31ff
Bartosz Miera <bartoszmiera@gmail.com>
2017-11-22 13:12:41 +0100

    Fixed any.hpp and added tests. Fixed CMakeLists.txt

---
+++ tests/any_test.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+
+#include "any.hpp"
+
+#include <vector>
+
+using namespace std;
+using namespace tpl;
+
+TEST_CASE( "Vector filtering", "[filtered_test]" ) {
+	vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	SECTION(" >= 1"){
+		const bool result = (v | any([](const auto &i){ return i >= 1; }));
+		REQUIRE(true == result);
+	}
+	SECTION(" > 10"){
+		const bool result = (v | any([](const auto &i){ return i > 10; }));
+		REQUIRE(false == result);
+	}
+}
