db342db0aa8a7fb4bb7a30569a9e5cec0173359d
Bartosz Miera <bartoszmiera@gmail.com>
2017-11-22 13:17:11 +0100

    Tests for all.hpp, fixed any.hpp tests.

---
+++ tests/all_test.cpp
@@ -0,0 +1,29 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+
+#include "all.hpp"
+
+#include <vector>
+
+using namespace std;
+using namespace tpl;
+
+TEST_CASE( "All in vector", "[all_test]" ) {
+	vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	SECTION(" >= 1"){
+		const bool result = (v | all([](const auto &i){ return i >= 1; }));
+		REQUIRE(true == result);
+	}
+	SECTION(" > 10"){
+		const bool result = (v | all([](const auto &i){ return i > 10; }));
+		REQUIRE(false == result);
+	}
+	SECTION(" > 5"){
+		const bool result = (v | all([](const auto &i){ return i > 5; }));
+		REQUIRE(false == result);
+	}
+	SECTION(" < 5"){
+		const bool result = (v | all([](const auto &i){ return i < 5; }));
+		REQUIRE(false == result);
+	}
+}
--- tests/any_test.cpp
+++ tests/any_test.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace tpl;
 
-TEST_CASE( "Vector filtering", "[filtered_test]" ) {
+TEST_CASE( "Any in vector", "[any_test]" ) {
 	vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	SECTION(" >= 1"){
 		const bool result = (v | any([](const auto &i){ return i >= 1; }));
@@ -17,5 +17,13 @@ TEST_CASE( "Vector filtering", "[filtered_test]" ) {
 	SECTION(" > 10"){
 		const bool result = (v | any([](const auto &i){ return i > 10; }));
 		REQUIRE(false == result);
+	}
+	SECTION(" > 5"){
+		const bool result = (v | any([](const auto &i){ return i > 5; }));
+		REQUIRE(true == result);
+	}
+	SECTION(" < 5"){
+		const bool result = (v | any([](const auto &i){ return i < 5; }));
+		REQUIRE(true == result);
 	}
 }
