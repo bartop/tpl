e5dd5aa73d5ea5de253d79e730aad1e41f2cf841
Bartosz Miera <bartoszmiera@gmail.com>
2017-11-27 19:26:04 +0100

    #12 Added copy_to operator.

---
+++ tests/copy_to_test.cpp
@@ -0,0 +1,30 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+
+#include <copy_to.hpp>
+
+#include <vector>
+
+using namespace std;
+using namespace tpl;
+
+TEST_CASE( "Copying into a vector", "[copy_to_test]" ) {
+	SECTION("Test 1"){
+		vector<int> in {1, 3, 5, 7, 9};
+		vector<int> out;
+		in | copy_to(std::back_inserter(out));
+		REQUIRE(in == out);
+	}
+	SECTION("Test 2"){
+		vector<int> in;
+		vector<int> out;
+		in | copy_to(std::back_inserter(out));
+		REQUIRE(in == out);
+	}
+	SECTION("Test 3"){
+		vector<int> in(5, 10);
+		vector<int> out;
+		in | copy_to(std::back_inserter(out));
+		REQUIRE(in == out);
+	}
+}
---
+++ tpl/copy_to.hpp
@@ -0,0 +1,46 @@
+
+#pragma once
+
+#include <iterator>
+
+namespace tpl{
+
+namespace detail{
+
+template<class Enumerable, class OutputIterator>
+void
+copy_to_function(Enumerable &&enumerable, OutputIterator &&outputIterator){
+	std::copy(std::begin(enumerable), std::end(enumerable), outputIterator);
+}
+
+}
+
+template<class OutputIterator>
+struct copy_to_iterator_holder {
+	explicit copy_to_iterator_holder(OutputIterator &&outputIterator) :
+		m_outputIterator(std::forward<OutputIterator>(outputIterator)){}
+
+	OutputIterator m_outputIterator;
+};
+
+template<class OutputIterator>
+copy_to_iterator_holder<OutputIterator>
+copy_to(OutputIterator &&outputIterator){
+	return copy_to_iterator_holder<OutputIterator>(
+		std::forward<OutputIterator>(outputIterator)
+	);
+}
+
+template<class Enumerable, class OutputIterator>
+void
+operator|(
+	Enumerable &&enumerable,
+   	copy_to_iterator_holder<OutputIterator> &&holder
+){
+	detail::copy_to_function(
+		std::forward<Enumerable>(enumerable),
+	   	std::forward<copy_to_iterator_holder<OutputIterator>>(holder).m_outputIterator
+	);
+}
+
+}
