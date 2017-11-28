3bb99042f46e56c0df588412a4f028f1fed84679
Bartosz Miera <bartoszmiera@gmail.com>
2017-11-28 16:23:42 +0100

    #15 Reduced ammount of copying. Fixed licence file location.

--- tpl/all.hpp
+++ tpl/all.hpp
@@ -7,8 +7,8 @@ namespace tpl{
 
 template<class LogicalPredicate>
 struct true_for_all_predicate_holder {
-	explicit true_for_all_predicate_holder(LogicalPredicate logicalPredicate) :
-		m_logicalPredicate(std::move(logicalPredicate)){}
+	explicit true_for_all_predicate_holder(LogicalPredicate &&logicalPredicate) :
+		m_logicalPredicate(std::forward<LogicalPredicate>(logicalPredicate)){}
 
 	LogicalPredicate m_logicalPredicate;
 };
@@ -16,9 +16,9 @@ struct true_for_all_predicate_holder {
 template<class Enumerable, class LogicalPredicate>
 class true_for_all {
 public:
-	true_for_all(Enumerable &&enumerable, LogicalPredicate logicalPredicate) :
+	true_for_all(Enumerable &&enumerable, LogicalPredicate &&logicalPredicate) :
 		m_enumerable(std::forward<Enumerable>(enumerable)),
-		m_logicalPredicate(std::move(logicalPredicate)){}
+		m_logicalPredicate(std::forward<LogicalPredicate>(logicalPredicate)){}
 
 	operator bool() const {
 		for(const auto &element : m_enumerable) {
@@ -41,19 +41,19 @@ private:
 
 template<class LogicalPredicate>
 true_for_all_predicate_holder<LogicalPredicate>
-all(LogicalPredicate logicalPredicate){
-	return true_for_all_predicate_holder<LogicalPredicate>(std::move(logicalPredicate));
+all(LogicalPredicate &&logicalPredicate){
+	return true_for_all_predicate_holder<LogicalPredicate>(std::forward<LogicalPredicate>(logicalPredicate));
 }
 
 template<class Enumerable, class LogicalPredicate>
 true_for_all<Enumerable, LogicalPredicate>
 operator|(
 	Enumerable &&enumerable,
-   	true_for_all_predicate_holder<LogicalPredicate> holder
+   	true_for_all_predicate_holder<LogicalPredicate> &&holder
 ){
 	return true_for_all<Enumerable, LogicalPredicate>(
 		std::forward<Enumerable>(enumerable),
-	   	std::move(holder.m_logicalPredicate)
+	   	std::forward<true_for_all_predicate_holder<LogicalPredicate>>(holder).m_logicalPredicate
 	);
 }
 
--- tpl/any.hpp
+++ tpl/any.hpp
@@ -7,8 +7,8 @@ namespace tpl{
 
 template<class LogicalPredicate>
 struct true_for_any_predicate_holder {
-	explicit true_for_any_predicate_holder(LogicalPredicate logicalPredicate) :
-		m_logicalPredicate(std::move(logicalPredicate)){}
+	explicit true_for_any_predicate_holder(LogicalPredicate &&logicalPredicate) :
+		m_logicalPredicate(std::forward<LogicalPredicate>(logicalPredicate)){}
 
 	LogicalPredicate m_logicalPredicate;
 };
@@ -16,9 +16,9 @@ struct true_for_any_predicate_holder {
 template<class Enumerable, class LogicalPredicate>
 class true_for_any {
 public:
-	true_for_any(Enumerable &&enumerable, LogicalPredicate logicalPredicate) :
+	true_for_any(Enumerable &&enumerable, LogicalPredicate &&logicalPredicate) :
 		m_enumerable(std::forward<Enumerable>(enumerable)),
-		m_logicalPredicate(std::move(logicalPredicate)){}
+		m_logicalPredicate(std::forward<LogicalPredicate>(logicalPredicate)){}
 
 	true_for_any &
 	operator=(true_for_any otherAny) {
@@ -47,19 +47,19 @@ private:
 
 template<class LogicalPredicate>
 true_for_any_predicate_holder<LogicalPredicate>
-any(LogicalPredicate logicalPredicate){
-	return true_for_any_predicate_holder<LogicalPredicate>(std::move(logicalPredicate));
+any(LogicalPredicate &&logicalPredicate){
+	return true_for_any_predicate_holder<LogicalPredicate>(std::forward<LogicalPredicate>(logicalPredicate));
 }
 
 template<class Enumerable, class LogicalPredicate>
 true_for_any<Enumerable, LogicalPredicate>
 operator|(
 	Enumerable &&enumerable,
-   	true_for_any_predicate_holder<LogicalPredicate> holder
+   	true_for_any_predicate_holder<LogicalPredicate> &&holder
 ){
 	return true_for_any<Enumerable, LogicalPredicate>(
 		std::forward<Enumerable>(enumerable),
-		std::move(holder.m_logicalPredicate)
+		std::forward<true_for_any_predicate_holder<LogicalPredicate>>(holder).m_logicalPredicate
 	);
 }
 
