9188c877891ae322d69cf7c00c4fe1ab8e606806
Bartosz Miera <bartoszmiera@gmail.com>
2017-12-02 01:05:20 +0100

    #7 Fold left without initial value added

--- tests/fold_left_test.cpp
+++ tests/fold_left_test.cpp
@@ -12,10 +12,14 @@ TEST_CASE( "Fold left initializer list", "[fold_left_test]" ) {
 	const auto v = { 1, 2, 3, 4, 5 };
 	int result = v | fold_left([](const auto &i, const auto &j){ return i+j;} , 0);
 	REQUIRE(15 == result);
+	result = v | fold_left([](const auto &i, const auto &j){ return i+j;} );
+	REQUIRE(15 == result);
 	result = v | fold_left([](const auto &i, const auto &j){ return i-j;} , 0);
 	REQUIRE(-15 == result);
 	result = v | fold_left([](const auto &i, const auto &j){ return i*j;} , 1);
 	REQUIRE(120 == result);
+	result = v | fold_left([](const auto &i, const auto &j){ return i*j;});
+	REQUIRE(120 != result);
 	result = v | fold_left([](const auto &i, const auto &j){ return i+j;} , 50);
 	REQUIRE(65 == result);
 	result = v | fold_left([](const auto &i, const auto &j){ return i+j;} , 150);
