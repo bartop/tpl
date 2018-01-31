c48b32f3c4b2afaf8ce5d359fd47fd6b4354cb15
Bartosz Miera <bartoszmiera@gmail.com>
2018-01-31 02:07:53 +0100

    Fixed #26 - added cycle generator that creates infinite cycling series

---
+++ tests/cycle_test.cpp
@@ -0,0 +1,43 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+
+#include <generator/cycle.hpp>
+
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+using namespace tpl::generator;
+
+TEST_CASE( "Testing cyclic sequence", "[cycle_test]" ) {
+	SECTION("1") {
+		const vector<int> a{1};
+		const auto result = cycle(a);
+		REQUIRE(1 == *result.begin());
+		REQUIRE(1 == *(++result.begin()));
+		std::vector<int> out;
+		const std::vector<int> expected { 1, 1, 1, 1, 1};
+		std::copy_n(result.begin(), 5, std::back_inserter(out));
+		REQUIRE(out == expected );
+	}
+
+	SECTION("1, 2") {
+		const auto a = {1, 2};
+		const auto result = cycle(a);
+		REQUIRE(1 == *result.begin());
+		REQUIRE(2 == *(++result.begin()));
+		REQUIRE(1 == *(++++result.begin()));
+		std::vector<int> out;
+		const std::vector<int> expected { 1, 2, 1, 2, 1};
+		std::copy_n(result.begin(), 5, std::back_inserter(out));
+		REQUIRE(out == expected );
+	}
+
+	SECTION("std::string(\"a\")") {
+		const vector<string> a{"a"};
+		const auto result = cycle(a);
+		REQUIRE((*result.begin() == "a"s));
+		REQUIRE((*(++result.begin()) == "a"s));
+	}
+}
---
+++ tpl/generator/cycle.hpp
@@ -0,0 +1,115 @@
+
+#pragma once
+
+#include "../meta/is_enumerable.hpp"
+#include "../meta/enumerable_traits.hpp"
+
+#include "../detail/iterator_base.hpp"
+
+#include <algorithm>
+
+namespace tpl{
+namespace generator{
+
+template<class Enumerable>
+class cycle_iterator : public detail::iterator_base<cycle_iterator<Enumerable>> {
+public:
+	using enumerable_traits = meta::enumerable_traits<typename std::decay<Enumerable>::type>;
+	using value_type = typename enumerable_traits::value_type;
+	using difference_type = ptrdiff_t;
+	using reference = const value_type &;
+	using pointer = const value_type *;
+	using iterator_category = std::input_iterator_tag;
+
+	~cycle_iterator() noexcept = default;
+
+	cycle_iterator(
+		Enumerable &&enumerable
+	) :
+		m_enumerable(std::forward<Enumerable>(enumerable)),
+   		m_currentIterator(enumerable.begin()) {}
+
+	cycle_iterator &
+	operator++() {
+		++m_currentIterator;
+		if (m_currentIterator == m_enumerable.end())
+			m_currentIterator = m_enumerable.begin();
+
+		return *this;
+	}
+
+	reference
+	operator*() const {
+		return *m_currentIterator;
+	}
+
+	pointer
+	operator->() const {
+		return m_currentIterator.operator->();
+	}
+
+	bool
+	operator==(const cycle_iterator &other) const {
+		return false;
+	}
+
+	void
+	swap(cycle_iterator &other) {
+		std::swap(this->m_enumerable, other.m_enumerable);
+	}
+
+private:
+	Enumerable m_enumerable;
+	typename enumerable_traits::const_iterator m_currentIterator;
+};
+
+template<class Enumerable>
+class cycle_sequence
+	: meta::enforce_enumerable<Enumerable> {
+public:
+	using value_type = typename meta::enumerable_traits<typename std::decay<Enumerable>::type>::value_type;
+	using const_iterator = cycle_iterator<Enumerable>;
+	using iterator = const_iterator;
+
+	cycle_sequence(
+		Enumerable &&enumerable
+	) :
+		m_enumerable(std::forward<Enumerable>(enumerable)){}
+
+	void
+	swap(cycle_sequence &other){
+		std::swap(m_enumerable, other.m_enumerable);
+	}
+
+	iterator
+	begin() {
+		return iterator(m_enumerable);
+	}
+
+	iterator
+	end() {
+		return iterator(m_enumerable);
+	}
+
+	const_iterator
+	begin() const {
+		return iterator(m_enumerable);
+	}
+
+	const_iterator
+	end() const {
+		return iterator(m_enumerable);
+	}
+
+private:
+	Enumerable m_enumerable;
+};
+
+template<class Enumerable>
+cycle_sequence<Enumerable>
+cycle(Enumerable &&enumerable){
+	return cycle_sequence<Enumerable>(std::forward<Enumerable>(enumerable));
+}
+
+}
+}
