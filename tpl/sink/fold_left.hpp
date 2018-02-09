7daff1392940914c36623feff82f257b6083e483
Bartosz Miera <bartoszmiera@gmail.com>
2018-02-09 23:09:09 +0100

    Fixed #41 - added default template parameters and removed redundant classes made for Visual Studio.

--- tpl/sink/fold_left.hpp
+++ tpl/sink/fold_left.hpp
@@ -1,13 +1,8 @@
 
 #include "../common/composite_factory.hpp"
 
-#ifndef _MSC_VER
-#include "fold_left_initialized.hpp"
 #include "fold_left_default.hpp"
-#else
-#include "fold_left_initialized_msvc.hpp"
-#include "fold_left_default_msvc.hpp"
-#endif
+#include "fold_left_initialized.hpp"
 
 namespace tpl{
 
