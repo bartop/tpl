a7eb2653264e0196a7290bf134da14602dc9b733
Bartosz Miera <bartoszmiera@gmail.com>
2018-02-10 18:27:23 +0100

    Fixed #44 - removed caching from sorted_sequence

--- tests/sorted_test.cpp
+++ tests/sorted_test.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace tpl;
 
-TEST_CASE( "Vector sorting", "[transformed_test]" ) {
+TEST_CASE( "Vector sorting", "[sorted_test]" ) {
 	vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	SECTION("i < j"){
 		const auto vf = (v | sort([](const auto &i, const auto &j){ return i < j; }));
@@ -21,6 +21,15 @@ TEST_CASE( "Vector sorting", "[transformed_test]" ) {
 		const auto vf = (v | sort(lambda));
 		vector<int> result(vf.begin(), vf.end());
 		std::sort(v.begin(), v.end(), lambda);
+		REQUIRE(v == result);
+	}
+
+
+	SECTION("Test lack of caching"){
+		const auto vf = (v | sort([](const auto &i, const auto &j){ return i < j; }));
+		vf.begin();
+		v = { 1, 2, 3 };
+		vector<int> result(vf.begin(), vf.end());
 		REQUIRE(v == result);
 	}
 }
