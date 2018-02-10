9e65c3f5d61271fa82afba3b794d195d5d743b0a
Bartosz Miera <bartoszmiera@gmail.com>
2018-02-10 21:48:11 +0100

    Fixed #50 - added grouping headers

---
+++ include/tpl/sink.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "sink/all.hpp"
+#include "sink/any.hpp"
+#include "sink/copy_to.hpp"
+#include "sink/count.hpp"
+#include "sink/fold_left.hpp"
+#include "sink/fold_left_default.hpp"
+#include "sink/fold_left_initialized.hpp"
